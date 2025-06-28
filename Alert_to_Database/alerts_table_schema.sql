5efc7d05bdf943b78ad12efa677cc7709de5b5a7
PRIYADHARSHAN VADIVEL <144985084+priyan37@users.noreply.github.com>
2025-06-28 12:20:49 +0530

    alerts_table_schema.sql

--- Alert_to_Database/alerts_table_schema.sql
+++ Alert_to_Database/alerts_table_schema.sql
@@ -1,1 +1,9 @@
-
+CREATE TABLE alerts (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    signature VARCHAR(255),
+    priority INT,
+    src_ip VARCHAR(45),
+    dst_ip VARCHAR(45),
+    message TEXT
+);
